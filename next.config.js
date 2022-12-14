4feac5b97e5338f01d5336922e7663fe477ee9bb
irinagrigoryeva <i.grigoryeva@hqsoftwarelab.com>
2022-12-14 23:02:08 +0300

    fix: fix next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // swcMinify: true,
-  images:{
-    domains:['i.pinimg.com','lh3.googleusercontent.com']
-  }
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    // swcMinify: true,
+    images: {
+        domains: ['i.pinimg.com', 'lh3.googleusercontent.com']
+    }
 }
 
 module.exports = nextConfig
